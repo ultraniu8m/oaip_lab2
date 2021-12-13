cdc00a4d20e638ef8741f88ed0f020fec8786d8a
ultraniu8m <95349360+ultraniu8m@users.noreply.github.com>
2021-12-13 10:00:21 +0300

    Add files via upload

---
+++ destant/destant/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace destant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            Text = "Hello World!";
+            this.BackColor = Color.Aquamarine;
+            this.Width = 250;
+            this.Height = 250;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
